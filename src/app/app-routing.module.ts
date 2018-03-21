20c4911b70d33e5a45f972d11c57888a785ebb9e
Daza Vegara <rdaza@indra.es>
2018-03-21 10:11:31 +0100

    project working

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/appComponent', pathMatch: 'full'
+  },
+  {
+    path: 'appComponent',
+    component: AppComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [],
+  providers: [],
+})
+export class AppRoutingModule { }
